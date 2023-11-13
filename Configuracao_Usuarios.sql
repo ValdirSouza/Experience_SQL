2481a4d464c97a62e1dcc81bb78c0d35463f039b
Valdir Souza <80654828+ValdirSouza@users.noreply.github.com>
2023-11-12 22:41:08 -0300

    Update Configuracao_Usuarios.sql
    Incluindo link de documentação e código

--- Configuracao_Usuarios.sql
+++ Configuracao_Usuarios.sql
@@ -4,9 +4,10 @@ CREATE USER 'username'@'localhost' IDENTIFIED BY '@Ab12345';
 -- Alteração de senha
 SET PASSWORD FOR 'username'@'localhost' = '$Cd67890';
 
+-- Reter configurações no servidor ao reiniciar
+FLUSH PRIVILEGES;
 
 -- LINK
 -- https://www.diegomacedo.com.br/administracao-de-usuarios-e-privilegios-no-banco-de-dados/
 -- https://www.digitalocean.com/community/tutorials/como-criar-um-novo-usuario-e-conceder-permissoes-no-mysql-pt
-
-FLUSH PRIVILEGES;
+-- https://dev.mysql.com/doc/refman/8.2/en/access-control.html
